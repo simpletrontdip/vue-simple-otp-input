006c2dd674da63eb5cee436bef2f8f585ab52f12
simpletron <thongitbk@gmail.com>
2022-03-07 14:29:11 +0700

    feat(demo): update demo for border effect with paste delay

--- src/App.vue
+++ src/App.vue
@@ -25,6 +25,20 @@
       <div><strong>Draft</strong> {{ otp }}</div>
       <div><strong>Submitted</strong> {{ submittedOtp }}</div>
     </section>
+    <section class="story">
+      <label>With extra slot to create border effect</label>
+      <SimpleOtpInput
+        class="otp-with-effect"
+        inputClasses="input-with-effect"
+        :pasteDelayMs="192"
+      >
+        <template v-slot:extra>
+          <span class="focus-border">
+            <i></i>
+          </span>
+        </template>
+      </SimpleOtpInput>
+    </section>
   </div>
 </template>
 
@@ -68,5 +82,75 @@ export default {
 .story {
   padding: 8px;
   margin-top: 24px;
+}
+
+.otp-with-effect .single-input-container {
+  border-radius: 3px;
+  overflow: hidden;
+}
+
+.input-with-effect {
+  outline: none;
+  border-width: 1px;
+  border-radius: 3px;
+}
+
+.input-with-effect:focus {
+  border-color: transparent;
+}
+
+.input-with-effect ~ .focus-border:before,
+.input-with-effect ~ .focus-border:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 0;
+  height: 2px;
+  background-color: green;
+  transition: 0.1s;
+  transition-delay: 0.2s;
+}
+.input-with-effect ~ .focus-border:after {
+  top: auto;
+  bottom: 0;
+  right: auto;
+  left: 0;
+  transition-delay: 0.6s;
+}
+.input-with-effect ~ .focus-border i:before,
+.input-with-effect ~ .focus-border i:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 2px;
+  height: 0;
+  background-color: green;
+  transition: 0.1s;
+}
+.input-with-effect ~ .focus-border i:after {
+  left: auto;
+  right: 0;
+  top: auto;
+  bottom: 0;
+  transition-delay: 0.2s;
+}
+.input-with-effect:focus ~ .focus-border:before,
+.input-with-effect:focus ~ .focus-border:after {
+  width: 100%;
+  transition: 0.1s;
+  transition-delay: 0.3s;
+}
+.input-with-effect:focus ~ .focus-border:after {
+  transition-delay: 0.1s;
+}
+.input-with-effect:focus ~ .focus-border i:before,
+.input-with-effect:focus ~ .focus-border i:after {
+  height: 100%;
+  transition: 0.1s;
+}
+.input-with-effect:focus ~ .focus-border i:after {
+  transition-delay: 0.2s;
 }
 </style>
